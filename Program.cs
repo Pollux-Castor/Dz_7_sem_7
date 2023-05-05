d0ff709223fa79faa6006c643d2708a4a6f2c966
Maxim_1623 <sabatag777@vivaldi.net>
2023-05-05 18:06:35 +0300

    Task 52 added

--- Program.cs
+++ Program.cs
@@ -44,7 +44,51 @@
 
 // 17 -> такого числа в массиве нет
 
-Console.WriteLine("Введите число строк");
+// Console.WriteLine("Введите число строк");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число столбцов");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// double [,] array = new double [rows,columns];
+
+// Console.WriteLine("Введите номер строки");
+// int rows2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите номер столбца");
+// int columns2 = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 0; i<array.GetLength(0); i++)
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         array[i,j] = new Random().Next(0,10);
+//         Console.Write(array[i,j]+ " | ");
+//     }
+
+// Console.WriteLine();
+// }
+// if (rows2<=rows+1 && columns2<=columns+1)
+// {
+// Console.WriteLine($"Значение элемента равно: " +array[rows2-1,columns2-1]);
+// }
+// else
+// {
+// Console.WriteLine("Cтроки: "+rows2+" ,или столбца: "+columns2+" в массиве нет");
+// }
+
+//-------------------------------------------------------------------------------------------
+
+// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+ Console.WriteLine("Введите число строк");
 int rows = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите число столбцов");
@@ -52,28 +96,26 @@ int columns = Convert.ToInt32(Console.ReadLine());
 
 double [,] array = new double [rows,columns];
 
-Console.WriteLine("Введите номер строки");
-int rows2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите номер столбца");
-int columns2 = Convert.ToInt32(Console.ReadLine());
-
 for (int i = 0; i<array.GetLength(0); i++)
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-        array[i,j] = new Random().Next(0,10);
-        Console.Write(array[i,j]+ " | ");
-    }
-
+ {
+     for (int j = 0; j < array.GetLength(1); j++)
+     {
+         array[i,j] = new Random().Next(0,10);
+         Console.Write(array[i,j]+ " | ");
+     }
 Console.WriteLine();
-}
-if (rows2<=rows+1 && columns2<=columns+1)
+ }
+ Console.WriteLine("***************");
+for (int j = 0; j < array.GetLength(1); j++)
 {
-Console.WriteLine($"Значение элемента равно: " +array[rows2-1,columns2-1]);
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+        sum += array[i, j];
+        }
+    Console.Write($"{ Math.Round(sum / array.GetLength(0),1)}"+"|");
 }
-else
-{
-Console.WriteLine("Cтроки: "+rows2+" ,или столбца: "+columns2+" в массиве нет");
-}
+Console.ReadLine();
+
+
 
